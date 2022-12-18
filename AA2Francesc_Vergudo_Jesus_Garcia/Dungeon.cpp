b814fadd104750dd5e78b63e1dede9ef91aee400
FrancescEnti <francesc.verdugo@enti.cat>
2022-12-18 12:34:19 +0100

    map

--- AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.cpp
+++ AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.cpp
@@ -1,5 +1,14 @@
 #include "Dungeon.h"
 
 void Dungeon::ShowDungeon() {
-
+	cout << " ___  ___  ___  ___  ___ \n";
+	for (int i = 0; i < 5; i++)
+	{
+		for (int x = 0; x < 5; x++)
+		{
+			cout << "|   |";
+			cout << "| " << MAP_RAW[i][x] << " |";
+			cout << "|___|";
+		}
+	}
 }
--- AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.h
+++ AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.h
@@ -1,7 +1,15 @@
 #pragma once
+#include "Player.h";
+#include "Logic.h";
 
 struct Dungeon {
-	char MAP_RAW[5][5];
+	char MAP_RAW[5][5] = {	{' ', ' ', ' ', ' ', ' '},
+							{' ', ' ', ' ', ' ', ' '},
+							{' ', ' ', ' ', ' ', ' '},
+							{' ', ' ', ' ', ' ', ' '},
+							{' ', ' ', ' ', ' ', ' '} };
 
+	void ShowHistory();
+	void ShowStats(Player player);
 	void ShowDungeon();
 };
