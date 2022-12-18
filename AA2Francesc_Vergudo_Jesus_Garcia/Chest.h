c01e2aa5a654ad5bf5c5e3a7c4432b6ea95d8c2a
JesusGarciaUB <jesus.garcia@enti.cat>
2022-12-18 13:51:17 +0100

    coses

--- AA2Francesc_Vergudo_Jesus_Garcia/Chest.h
+++ AA2Francesc_Vergudo_Jesus_Garcia/Chest.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "MapPosition.h"
+#include "Gear.h"
 
 struct Chest {
 	MapPosition pos;
+	int gold;
+	bool isLooted;
+	bool containsPotion;
+	Gear gear;
+
+	void Init();
 };
---
+++ AA2Francesc_Vergudo_Jesus_Garcia/Gear.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Gear {
+	string name;
+	int value;
+	int HP = 0;
+	int stamina = 0;
+	int agility = 0;
+};
