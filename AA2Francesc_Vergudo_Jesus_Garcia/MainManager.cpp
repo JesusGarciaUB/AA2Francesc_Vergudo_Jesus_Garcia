f51a5abcac2e3d9c8c73906c3aff7823349e54df
PC-imatge-2022-2023 <licenses@enti.cat>
2022-12-19 14:27:49 +0100

    A

--- AA2Francesc_Vergudo_Jesus_Garcia/MainManager.cpp
+++ AA2Francesc_Vergudo_Jesus_Garcia/MainManager.cpp
@@ -168,7 +168,7 @@ void MainManager::CombatScene() {
 
 void MainManager::ChestScene() {
 
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < 2; i++)
 	{
 		if (player.pos.x == chests[i].pos.x && player.pos.y == chests[i].pos.y) {
 
