4e0b4ed59153bf1ec0a6c62abf38cb884349256a
FrancescEnti <licenses@enti.cat>
2022-12-20 10:45:26 +0100

    coment dungeon

--- AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.h
+++ AA2Francesc_Vergudo_Jesus_Garcia/Dungeon.h
@@ -7,10 +7,28 @@ struct Dungeon {
 							{' ', ' ', ' ', ' ', ' '},
 							{' ', ' ', ' ', ' ', ' '},
 							{' ', ' ', ' ', ' ', ' '} };
-
+	/// <summary>
+	/// Muestra la parte superior del menu de DUNGEON
+	/// </summary>
 	void ShowTopMenu();
+
+	/// <summary>
+	/// Muestra el mapa de la dungeon
+	/// </summary>
 	void ShowDungeon();
+
+	/// <summary>
+	/// Muestra la parte inferior del menu de DUNGEON
+	/// </summary>
 	void ShowBottomMenu();
+
+	/// <summary>
+	/// Comprueba si hay o no un muro cuando el jugador se mueve
+	/// </summary>
 	bool IsThereAWall(int x, int y, char key);
+
+	/// <summary>
+	/// Comprueba si hay un enemigo o un cofre cuando el jugador se mueve
+	/// </summary>
 	char IsThereAnEvent(int x, int y, char key);
 };
