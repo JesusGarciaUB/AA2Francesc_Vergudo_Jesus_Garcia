7f38415f449722104c59b498a9dea80b5c560cf9
FrancescEnti <licenses@enti.cat>
2022-12-20 10:42:03 +0100

    comentaris logica, mainmanager, player

--- AA2Francesc_Vergudo_Jesus_Garcia/Logic.h
+++ AA2Francesc_Vergudo_Jesus_Garcia/Logic.h
@@ -7,5 +7,12 @@
 
 using namespace std;
 
+/// <summary>
+///	En la logica tenemos los includes que se usan en multiples archivos.
+/// 
+/// getRandom() duvuelve un un rango de numeros y devuelve un random entre ese rango / o le puedes pasar un valor maximo y te retornara un valor entre 1 y el valor maximo
+///  
+/// </summary>
+
 int getRandom(int min, int max);
 int getRandom(int max);
--- AA2Francesc_Vergudo_Jesus_Garcia/Player.h
+++ AA2Francesc_Vergudo_Jesus_Garcia/Player.h
@@ -18,6 +18,14 @@ struct Player {
 	int potions;
 	int maxPotions;
 
+	/// <summary>
+	///  Initialize() inicializa un jugador
+	/// 
+	///	 UsePotion() comprueba si el juegador tiene pociones y si tiene usa una.
+	/// 
+	/// ShowStats() muestra las estadisticas base del jugador (Vida, pociones y movimientos). Se usa para printearlo en la interfaz de dungeon
+	/// </summary>
+
 	void Initialize();
 	void UsePotion();
 	void ShowStats();
